b33f16e1e0bdab3a747e8c999811eab5329f7906
John-Flag <john.crockett@flagstaffaz.gov>
2023-09-25 17:21:47 -0700

    Fixed unboound local variable error

--- lf_analyzer.py
+++ lf_analyzer.py
@@ -3,11 +3,12 @@ import logging
 import os.path
 import uuid
 import pyodbc
-#import datetime
+import sys
 
-## *** Restart as Administrator *** ##
+## *** Run as Administrator *** ##
 
-## gets called by walk(), retrieves the paths for all the pages assoc with a particular document (tocid)
+## gets called by walk(), retrieves the paths for all the pages assoc with 
+## a particular document (tocid)
 def get_page_path(cnxn, tocid):
     logging.debug('Getting file paths for tocid = ' + str(tocid))
 
@@ -50,7 +51,10 @@ def get_page_path(cnxn, tocid):
         logging.debug('Pages SQL executed')
         pages = cu.fetchall()
 
-    return pages
+    if not pages == []:
+        return pages
+    else:
+        return 'empty'
 
 
 def get_metadata(cnxn, tocid):
@@ -73,7 +77,10 @@ def get_metadata(cnxn, tocid):
                     "propval pv, propdef pd ")
     sql_where = ("WHERE " +
                     "pv.tocid = " + str(tocid) + " AND " +
-                    "pv.prop_id = pd.prop_id;")
+                    "pv.prop_id = pd.prop_id " +
+                    "AND (pv.str_val IS NOT NULL " +
+		              "OR pv.num_val IS NOT NULL " +
+                      "OR pv.date_val IS NOT NULL);")
     
     sql = sql_select + sql_from + sql_where
     logging.debug(sql)
@@ -91,13 +98,11 @@ def get_metadata(cnxn, tocid):
 def walk(cnxn, obj_id = "NULL"):
     
     try:
-        logging.debug('Begin walk() iteration for container ' + str(obj_id))
+        logging.info('Begin walk() iteration for container ' + str(obj_id))
 
         ## Initialize the lists and variables needed to 
         ## collect the data
-        doc_list = []
-        container_list = []
-        results = []
+        docs_n_containers = []
             
         sql_select = "SELECT tocid, etype "
         sql_from = "FROM toc "
@@ -121,96 +126,77 @@ def walk(cnxn, obj_id = "NULL"):
 
             ## Once you have a resultset, transfer it into a list so the
             ## "with block" wil dispose of it when done
-            results = cu.fetchall()
+            docs_n_containers = cu.fetchall()
             logging.debug('Tree rows fetched')
                 
-            for row in results:
+            for row in docs_n_containers:
                 if row[1] == -2:  #Document
                     ## DO DOCUMENT STUFF (grab pages, paths, and keywords) ##
                     ## get_page_path returns a list of lists:
                     ## [tocid, doc_name, page_num, path_name]
-                    ## TODO:  Something is happening here with the error
-                    ## 'cannot access local variable 'doc_pages_n_paths' where it is not associated with a value'
-                    ## Suspect get_page_path() is returning nothing.
-                    ## Need to test for a "nothing" return and exclude it from doing anything
-                    ## On second thought the exception block did not catch it, so it may be 
-                    ## happening at line 162
-                    doc_pages_n_paths = get_page_path(cnxn, row[0])
+                    with open('pages.csv', 'a', newline='') as f:
+                        csv_w = csv.writer(f)
+                        csv_w.writerows(get_page_path(cnxn, row[0]))
 
                     ## get_page_path returns a list of lists:
                     ## [tocid, key_name, key_val]
-                    kwds = get_metadata(cnxn, row[0])
+                    with open('kwds.csv', 'a', newline='') as f:
+                        csv_w = csv.writer(f)
+                        csv_w.writerows(get_metadata(cnxn, row[0]))
                     
                 elif row[1] == 0: #Container
                     # Do container stuff
                     walk(cnxn, row[0])
 
                 else:
-                    #TODO: etype is that one "-1."  Not sure yet what to do with 
-                    #      that one.  Maybe ignore since it's like 0.00001%
+                    #TODO: status = undecided
+                    ## etype is that one "-1."  Not sure yet what to do with 
+                    #  that one.  Maybe ignore since it's like 0.00001%
                     pass
 
-            ## TODO: All of these inert blank lines into the file
-            ## Work out why and eliminate the issue.
-            ## Also, in keywords, it should only record records that actually 
-            ## have keywords and not record lines where there is no kwd
-            with open('results.csv', 'a') as f:
-                csv_w = csv.writer(f)
-                csv_w.writerows(results)
-
-            try:
-                with open('pages.csv', 'a') as f:
-                    csv_w = csv.writer(f)
-                    csv_w.writerows(doc_pages_n_paths)
-            except Exception as ve:
-                print(ve)
-
-            with open('doc_kwds.csv', 'a') as f:
-                csv_w = csv.writer(f)
-                csv_w.writerows(kwds)
-
     except Exception as e:
-        logging.debug(str(e))
-        print(e)
-  
+        logging.warning(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        logging.warning(exc_type, exc_tb.tb_lineno)
 ## ------------- End of Functions ----------------------------
 
 
 ## ----------------- Script ---------------------------
-logging_level = logging.info
-log_file_name = os.path.join('.', str(uuid.uuid1()) + '.log')
-logging.basicConfig(filename=log_file_name, 
-                    filemode='w', 
-                    encoding='utf-8', 
-                    format='%(asctime)s.%(msecs)03d %(message)s', 
-                    datefmt='%Y-%m-%d %H:%M:%S', 
-                    level=logging_level)
+try:
+    log_file_name = os.path.join('.',str(uuid.uuid1()) + '.log')
+    logging.basicConfig(filename=log_file_name, 
+                        filemode='w', 
+                        encoding='utf-8', 
+                        format='%(asctime)s.%(msecs)03d %(message)s', 
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.getLevelName('WARNING'))
 
-logging.info("Walk begins")
+    logging.info("Walk begins")
 
-dsn_string = "DSN=LaserFicheDb"
+    start_container_tocid = 1
+    dsn_string = "DSN=LaserFicheDb"
+    output_filename = "TestRun003"
+    odbc_ver = "ODBC Driver 18 for SQL Server"
+    trust_srvr_cert = "Yes"
+    database = "Laser8"
+    # mars = "Yes"
+    trusted_cnxn = "Yes"
+    server = "city-db2\laserf"
 
-output_filename = "TestRun003"
+    cnxn_str = ("SERVER=" + server + "; " + 
+                "DATABASE=" + database + "; " + 
+                "DRIVER=" + odbc_ver + "; " + 
+                "Trusted_Connection=" + trusted_cnxn + "; " + 
+                "TrustServerCertificate=" + trust_srvr_cert + ";")
 
-odbc_ver = "ODBC Driver 18 for SQL Server"
-trust_srvr_cert = "Yes"
-database = "Laser8"
-mars = "Yes"
-trusted_cnxn = "Yes"
-server = "city-db2\laserf"
+    ## Instantiate a connection that can be passed to walk()
+    cnxn = pyodbc.connect(cnxn_str)
 
-cnxn_str = ("SERVER=" + server + "; " + 
-            "DATABASE=" + database + "; " + 
-            "DRIVER=" + odbc_ver + "; " + 
-            "Trusted_Connection=" + trusted_cnxn + "; " + 
-            "TrustServerCertificate=" + trust_srvr_cert + "; " + 
-            "MultipleActiveResultSets=" + mars)
+    walk(cnxn, start_container_tocid)
 
-## Instantiate a connection that can be passed to walk()
-cnxn = pyodbc.connect(cnxn_str)
+    logging.info('Walk ends')
 
-start_container_tocid = 1
-
-walk(cnxn, start_container_tocid)
-
-logging.info('Walk ends')
+except Exception as se:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    logging.warning(se)
+    logging.warning(exc_type, exc_tb.tb_lineno)
